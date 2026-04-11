2f3d961f7aa2f5c7069e82f09fe53a8242cf9268
M Muhtasim Ridwan <ridwanmuhtasim@gmail.com>
2026-04-10 22:04:56 -0400

    04.09 : Tax Problem

--- 4.9/problem1.py
+++ 4.9/problem1.py
@@ -12,14 +12,14 @@
 # You must handle priority correctly.
 
 # Answer:
-n = int(input("Enter your number : "))
+# n = int(input("Enter your number : "))
 
-if n % 3 == 0 and n % 5 == 0 :
-    print("FizzBuzz")
-elif n % 3 == 0 and n % 5 != 0 :
-    print("Fizz")
-elif n % 3 != 0 and n % 5 == 0 :
-    print("Buzz")
+# if n % 3 == 0 and n % 5 == 0 :
+#     print("FizzBuzz")
+# elif n % 3 == 0 and n % 5 != 0 :
+#     print("Fizz")
+# elif n % 3 != 0 and n % 5 == 0 :
+#     print("Buzz")
     
     
 # Expert :
@@ -31,4 +31,21 @@ elif n % 3 != 0 and n % 5 == 0 :
 #     result += "Buzz"
 
 # print(result or n)
+
+def FizzBuzz(n) :
+    if n <= 0 :
+        print("Cant be 0")
+    elif n % 3 == 0 and n % 5 == 0 :
+        print("FizzBuzz")
+    elif n % 3 == 0 :
+        print("Fizz")
+    elif n % 5 == 0 :
+        print("Buzz")
+    else :
+        print("Not Fizz or Buzz")
     
+FizzBuzz(15)
+FizzBuzz(5)
+FizzBuzz(3)
+FizzBuzz(7)
+FizzBuzz(0)
